df83d14c0ab2ba40413d781c06c98536d68d6314
Runar Borge <runar.borge@uninett.no>
2018-08-28 10:52:54 +0200

    Migrated monitor.py to a real monitoring display command

--- monitor.py
+++ monitor.py
@@ -2,20 +2,12 @@ from ritz import ritz, notifier, parse_config
 from pprint import pprint
 from os.path import expanduser
 from time import sleep
+from termcolor import cprint
 import re
 import argparse
 import sys
 import logging
-
-
-def importconf(file):
-  config = {}
-  with open(expanduser(file), "r") as f:
-    for line in f.readlines():
-      sets = re.findall("^\s?set\s+(\S+)\s+(.*)$", line)
-      if sets:
-        config[sets[0][0]] = sets[0][1]
-  return config
+import sys
 
 
 cases = {}
@@ -23,19 +15,25 @@ cases = {}
 
 
 def show():
-  print(chr(27) + "[2J")      # Clear screen
+  # print(chr(27) + "[2J")      # Clear screen
+  print("\033c")              #Clear screen
   for c in cases:
     case = cases[c]
+    if case["state"] in ("ignored","working", "waiting","closed"):
+        continue
     if "portstate" in case["type"]:
       # LowerLayerDown state is quite long... shorten it
       if "lowerLayerDown" in case["portstate"]:
         portState = "LowerDown"
       else:
         portState = case["portstate"]
-      
-      print("%-10s %-8s %-15s %-24s %-s" % (
+
+      cprint("%-10s %-8s %-15s %-24s %-s" % (
             portState, case["state"], case["router"],
-            case["port"], case["descr"] if "descr" in case else "---"))
+            case["port"], case["descr"] if "descr" in case else "---"),"yellow")
+
+    elif "reachability" in case["type"]:
+      cprint(" DEVICE DOWN: %s" % case["router"], "red")
 
 def main():
   parser = argparse.ArgumentParser(description='Process some integers.')
@@ -53,7 +51,7 @@ def main():
     c_server = conf["UNINETT-backup"]["Server"]
     c_user   = conf["UNINETT-backup"]["User"]
     c_secret = conf["UNINETT-backup"]["Secret"]
-    
+
   ritzlog = logging.getLogger("ritz")
   ritzlog.setLevel(logging.DEBUG)
   ritzlog.addHandler(logging.FileHandler('comm.log'))
@@ -61,11 +59,13 @@ def main():
   sess.connect()
   sess.authenticate(c_user, c_secret)
   get_caseids = sess.get_caseids()
-
+  print("Collecting cases:")
   for i in get_caseids:
-    print("Case: %i" % i)
+    sys.stdout.write(" %i" % i)
+    sys.stdout.flush()
     cases[i] = sess.get_attributes(i)
 
+  sys.stdout.write("\n\n")
   show()
 
   pass
@@ -78,33 +78,20 @@ def main():
       print(n)
       p = n.split(' ', 2)
       if "attr" in p[1]:
-        pprint(sess.get_attributes(int(p[0])))
-      elif "log" in p[1]:
-        pprint(sess.get_log(int(p[0])))
+        case = int(p[0])
+        cases[case] = sess.get_attributes(case)
+        #pprint(sess.get_attributes(int(p[0])))
+      #elif "log" in p[1]:
+      #  pprint(sess.get_log(int(p[0])))
       elif "state" in p[1]:
         v = p[2].split(' ', 1)
         print("State on %s changed from %s to %s" % (p[0], v[0], v[1]))
       else:
-        print("UNKNOWN!!!!  : %s" % n)
+        print("UNKNOWN EVENT !!!!  : %s" % n)
 
       print()
     sleep(1)
 
-
-  if args.CaseID not in get_caseids:
-    print(get_caseids)
-    print("CaseID %i is not in database" % args.CaseID)
-    sys.exit(1)
-
-  pprint(sess.get_attributes(args.CaseID))
-  print("Get History: %i" % args.CaseID)
-  pprint(sess.gethist(args.CaseID))
-  print("Get Log: %i" % args.CaseID)
-  pprint(sess.get_log(args.CaseID))
-  # print("Add test message to log: 24014")
-  # pprint(sess.add_history(24014, "Testmelding ifra pyRitz"))
-  # print("set_state 'open': %i" % args.CaseID)
-  # pprint(sess.set_state(args.CaseID, "open"))
 
 
 if __name__ == "__main__":
