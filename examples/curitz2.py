99e26768d25073cef9c06c7a2a4e67f4a707142b
Kolbjørn Barmen <kolbjorn.barmen@uninett.no>
2018-11-16 11:08:27 +0100

    Work-around for non-color terminal

--- examples/curitz2.py
+++ examples/curitz2.py
@@ -122,9 +122,15 @@ def main(screen):
     screen.keypad(1)
     screen.timeout(1 * 1000)  # mSec timeout
 
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_CYAN)
-    curses.init_pair(10, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(11, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    try:
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_CYAN)
+        curses.init_pair(10, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(11, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
+    except curses.error:
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(11, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     curses.curs_set(0)
     screen_size = BoxSize(*screen.getmaxyx())
