5cf2f2e2a36bfdbc7486b091efe945014f10854a
Kolbjørn Barmen <kolbjorn.barmen@uninett.no>
2018-11-20 14:05:03 +0100

    Away with warnings, fix version depend

--- setup.py
+++ setup.py
@@ -6,18 +6,13 @@ setup(
     author='Runar Borge',
     author_email='runar.borge@uninett.no',
     packages=find_packages(),
-    python_requires='<=3.5',
+    python_requires='>=3.5',
     url=[''],
     license='LISENSE.txt',
     description="Python interface to Zino",
     long_description=open('README.md').read(),
     include_package_data=True,
     install_requires=[],
-    CLASSIFIERS=[
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-    ],
     scripts=['bin/curitz'],
     py_modules=['culistbox']
 )
