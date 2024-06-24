970cc681e266cf5d038a70181d1b95a7a0044bbf
Hanne Moa <hanne.moa@sikt.no>
2024-06-24 11:50:54 +0200

    Add support for TCP keepalive
    Add configurable support for TCP keepalive for protocol port (8001) and
    ntie/updates port (8002).
    
    Turned on by default, via a flag "keepalive" on __init__.

--- src/zinolib/utils.py
+++ src/zinolib/utils.py
@@ -1,10 +1,13 @@
 import functools
 import hashlib
+import platform
+import socket
 
 
 __all__ = [
     "windows_codepage_cp1252",
     "generate_authtoken",
+    "enable_socket_keepalive",
 ]
 
 
@@ -80,3 +83,42 @@ def log_exception_with_params(logger, reraise=True, return_value=None):
                 return return_value
         return wrapper
     return inner
+
+
+def _enable_keepalive_linux(sock, after_idle_sec, interval_sec, max_fails):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+
+
+def _enable_keepalive_osx(sock, after_idle_sec, interval_sec, max_fails):
+    """Set TCP keepalive on an open socket.
+
+    sends a keepalive ping once every 3 seconds (interval_sec)
+    """
+    # scraped from /usr/include, not exported by python's socket module
+    TCP_KEEPALIVE = 0x10
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval_sec)
+
+
+def _enable_keepalive_win(sock, after_idle_sec, interval_sec, max_fails):
+    sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, after_idle_sec * 1000, interval_sec * 1000))
+
+
+def enable_socket_keepalive(sock, after_idle_sec=60, interval_sec=60, max_fails=5):
+    platforms = {
+        "Linux": _enable_keepalive_linux,
+        "Darwin": _enable_keepalive_osx,
+        "Windows": _enable_keepalive_win,
+    }
+    if (plat := platform.system()) in platforms:
+        return platforms[plat](sock, after_idle_sec, interval_sec, max_fails)
+    raise RuntimeError('Unsupported platform: {}'.format(plat))
